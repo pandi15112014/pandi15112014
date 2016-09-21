f4424206c685d4e8d61e784c298e96bead76f7e0
pandi2312 <Pandiyarajan.Ganapathy@xerox.com>
2016-09-21 11:26:37 +0530

    Payment Table script updated

--- src/main/resources/database/dbinit.sql
+++ src/main/resources/database/dbinit.sql
@@ -1,21 +1,20 @@
 
 
-drop table payments if exists;
-drop table accounts if exists;
-
-create table accounts (
+create table IF NOT EXISTS accounts (
   ID BIGINT NOT NULL PRIMARY KEY,
   NAME VARCHAR(100),
   BALANCE DECIMAL (8,2)
 );
 
-create table payments (
-   ID BIGINT IDENTITY NOT NULL PRIMARY KEY,
-   PAYEE BIGINT NOT NULL,
-   RECIPIENT BIGINT NOT NULL,
-   AMOUNT DECIMAL (8,2),
-   PAY_DATE DATE
-);
+CREATE TABLE IF NOT EXISTS `payments` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `PAYEE` bigint(20) NOT NULL DEFAULT '0',
+  `RECIPIENT` bigint(20) NOT NULL DEFAULT '0',
+  `AMOUNT` decimal(8,2) DEFAULT '0.00',
+  `PAY_DATE` date DEFAULT NULL,
+  KEY `payment_id` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 INSERT INTO ACCOUNTS (ID, NAME, BALANCE) VALUES (1, 'Airline1', 0);
